ad7b7f3b577d8b666a32024da16512e526096e6d
saikiranchary <gattojisaikumar@94gmail.com>
2023-07-11 13:28:28 +0530

    commiting java code

---
+++ src/com/slokam/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.slokam.controller;
+
+public class Controller {
+
+	public static void testcontroller() {
+		System.out.println("controller menthod");
+	}
+	
+	public static void main(String[] args) {
+	Controller con = new Controller();
+	testcontroller();
+}
+
+}
